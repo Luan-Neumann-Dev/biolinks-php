111ebc979b90236b7b34efb2b41d51ded7d4c7b6
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2025-11-05 20:06:35 -0300

    chore: create login route and form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,13 @@
 <div>
     <h1>Login</h1>
+
+    <form action="/login" method="post">
+        @csrf
+
+        <input name="email" placeholder="email" value="{{old('email')}}" />
+
+        <input name="password" type="password" placeholder="senha" />
+
+        <button>Logar</button>
+    </form>
 </div>
