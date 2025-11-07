270d9bd619b348ba9d7d1c1a836ddc3926c8805b
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2025-11-06 21:01:09 -0300

    feat: create link

---
+++ database/factories/LinkFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Link;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class LinkFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+        ];
+    }
+}
