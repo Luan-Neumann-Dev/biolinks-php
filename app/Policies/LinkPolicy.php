fcfa9979c63f1c0d99f3b65dc23e91d012114c55
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2025-11-11 19:09:53 -0300

    chore: add policies

---
+++ app/Policies/LinkPolicy.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Link;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class LinkPolicy
+{
+    public function atualizar(User $user, Link $link): Response
+    {
+        return $link->user->is($user) ? Response::allow() : Response::deny("Este link não é seu");
+    }
+}
