ce1f8fa065a721a53aafbcd0a78bb0e5e630eec7
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2025-11-12 20:52:30 -0300

    chore: using all the components

---
+++ resources/views/components/textarea.blade.php
@@ -0,0 +1,10 @@
+@props(['value'])
+
+<div>
+    <textarea {{ $attributes }} class="textarea w-full">
+        {{ $value }}
+    </textarea>
+    @error('description')
+        <span>{{ $message }}</span>
+    @enderror
+</div>
