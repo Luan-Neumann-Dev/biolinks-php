fa6e573f4552fd63eb9d64fc3cc205c23a982e43
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2025-11-12 21:25:47 -0300

    chore: using components

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,21 @@
-<button {{$attributes->class(['btn btn-primary'])}}>
+@props([
+    'href' => null,
+    'block' => null,
+    'outline' => null,
+    'color' => 'primary'
+])
+
+@php
+    $tag = $href ? 'a' : 'button';
+@endphp
+
+<{{ $tag }} {{ $href ? "href=$href" : '' }}
+    {{ $attributes->class([
+        'btn',
+        "btn-{$color}",
+        'btn-block' => $block,
+        'btn-outline' => $outline,
+    ])
+}}>
     {{ $slot }}
-</button>
+</{{ $tag }}>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,46 +1,52 @@
 <x-layout.app>
-    <div>
-        <h1>Dashboard</h1>
+    <x-container>
+        <div class="text-center space-y-4">
+            <x-img src="storage/{{ $user->photo }}" alt="Profile Picture" />
 
-        <h2>User {{ auth()->user()->name }} :: {{ auth()->id() }}</h2>
+            <div class="font-bold text-2xl tracking-wide">{{ $user->name }}</div>
 
-        <a href="{{ route('profile') }}">Atualizar perfil</a>
+            <div class="font-sm opacity-80">{{ $user->description }}</div>
 
-        @if($message = session()->get('message'))
-            <div>{{ $message }}</div>
-        @endif
+            <ul class="space-y-2">
+                @foreach ($links as $link)
+                    <li class="flex items-center gap-2">
+                        @unless ($loop->last)
+                            <x-form :route="route('links.down', $link)" patch>
+                                <x-button color="ghost">
+                                    <x-icons.arrow-down class="w-6 h-6" />
+                                </x-button>
+                            </x-form>
+                        @else
+                            <x-button disabled color="ghost">
+                                <x-icons.arrow-down class="w-6 h-6" />
+                            </x-button>
+                        @endunless
 
-        <a href="{{route('links.create')}}">Criar novo link</a>
+                        @unless ($loop->first)
+                            <x-form :route="route('links.up', $link)" patch>
+                                <x-button color="ghost">
+                                    <x-icons.arrow-up class="w-6 h-6" />
+                                </x-button>
+                            </x-form>
+                        @else
+                            <x-button disabled color="ghost">
+                                <x-icons.arrow-up class="w-6 h-6" />
+                            </x-button>
+                        @endunless
 
-        <ul>
-            @foreach ($links as $link)
-                <li style="display: flex;">
-                    @unless($loop->last)
-                        <form action="{{ route('links.down', $link) }}" method="post">
-                            @csrf
-                            @method('PATCH')
-                            <button>⬇️</button>
-                        </form>
-                    @endunless
+                        <x-button href="{{ route('links.edit', $link) }}" block outline color="info">
+                            {{ $link->name }}
+                        </x-button>
 
-                    @unless ($loop->first)
-                        <form action="{{ route('links.up', $link) }}" method="post">
-                            @csrf
-                            @method('PATCH')
-                            <button>⬆️</button>
-                        </form>
-                    @endunless
+                        <x-form :route="route('links.destroy', $link)" delete onsubmit="return confirm('Tem certeza?')">
 
-                    <a href="{{route('links.edit', $link)}}">{{ $link->name }}</a>
-
-                    <form action="{{route('links.destroy', $link)}}" method="post"
-                          onsubmit="return confirm('Tem certeza?')">
-                        @csrf
-                        @method('DELETE')
-                        <button>Deletar</button>
-                    </form>
-                </li>
-            @endforeach
-        </ul>
-    </div>
+                            <x-button color="ghost">
+                                <x-icons.trash class="w-6 h-6" />
+                            </x-button>
+                        </x-form>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    </x-container>
 </x-layout.app>
